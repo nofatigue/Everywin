a5124a518331bfa9c32d54b0853b2472db30980f
om <nofatigua@gmail.com>
2019-01-12 15:35:52 +0200

    Use ObjectListView - much better

--- Everywin/Form1.Designer.cs
+++ Everywin/Form1.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.search_bar = new System.Windows.Forms.TextBox();
-            this.WindowsListbox = new System.Windows.Forms.ListBox();
+            this.windows_olv = new BrightIdeasSoftware.ObjectListView();
+            this.colmun1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.column2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            ((System.ComponentModel.ISupportInitialize)(this.windows_olv)).BeginInit();
             this.SuspendLayout();
             // 
             // search_bar
@@ -40,30 +43,56 @@
             this.search_bar.TabIndex = 0;
             this.search_bar.TextChanged += new System.EventHandler(this.search_bar_TextChanged);
             this.search_bar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.search_bar_KeyDown);
+            this.search_bar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.search_bar_KeyPress);
+            this.search_bar.KeyUp += new System.Windows.Forms.KeyEventHandler(this.search_bar_KeyUp);
             // 
-            // WindowsListbox
+            // windows_olv
             // 
-            this.WindowsListbox.ColumnWidth = 5;
-            this.WindowsListbox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.WindowsListbox.FormattingEnabled = true;
-            this.WindowsListbox.ItemHeight = 20;
-            this.WindowsListbox.Location = new System.Drawing.Point(12, 38);
-            this.WindowsListbox.Name = "WindowsListbox";
-            this.WindowsListbox.Size = new System.Drawing.Size(776, 384);
-            this.WindowsListbox.TabIndex = 1;
-            this.WindowsListbox.SelectedIndexChanged += new System.EventHandler(this.WindowsListbox_SelectedIndexChanged);
-            this.WindowsListbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.WindowsListbox_KeyDown);
+            this.windows_olv.AllColumns.Add(this.colmun1);
+            this.windows_olv.AllColumns.Add(this.column2);
+            this.windows_olv.CellEditUseWholeCell = false;
+            this.windows_olv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colmun1,
+            this.column2});
+            this.windows_olv.Cursor = System.Windows.Forms.Cursors.Default;
+            this.windows_olv.HideSelection = false;
+            this.windows_olv.Location = new System.Drawing.Point(12, 38);
+            this.windows_olv.Name = "windows_olv";
+            this.windows_olv.ShowGroups = false;
+            this.windows_olv.Size = new System.Drawing.Size(776, 476);
+            this.windows_olv.TabIndex = 2;
+            this.windows_olv.UseCompatibleStateImageBehavior = false;
+            this.windows_olv.UseFiltering = true;
+            this.windows_olv.View = System.Windows.Forms.View.Details;
+            this.windows_olv.SelectedIndexChanged += new System.EventHandler(this.windows_olv_SelectedIndexChanged);
+            this.windows_olv.KeyDown += new System.Windows.Forms.KeyEventHandler(this.windows_olv_KeyDown);
+            this.windows_olv.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.windows_olv_KeyPress);
+            this.windows_olv.MouseClick += new System.Windows.Forms.MouseEventHandler(this.windows_olv_MouseClick);
+            // 
+            // colmun1
+            // 
+            this.colmun1.AspectName = "Image";
+            this.colmun1.ImageAspectName = "ProcessPath";
+            this.colmun1.Text = "image";
+            this.colmun1.Width = 118;
+            // 
+            // column2
+            // 
+            this.column2.AspectName = "Title";
+            this.column2.FillsFreeSpace = true;
+            this.column2.Text = "title";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 429);
-            this.Controls.Add(this.WindowsListbox);
+            this.ClientSize = new System.Drawing.Size(800, 526);
+            this.Controls.Add(this.windows_olv);
             this.Controls.Add(this.search_bar);
             this.Name = "Form1";
             this.Text = "Everywin";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.windows_olv)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,7 +101,9 @@
         #endregion
 
         private System.Windows.Forms.TextBox search_bar;
-        private System.Windows.Forms.ListBox WindowsListbox;
+        private BrightIdeasSoftware.ObjectListView windows_olv;
+        private BrightIdeasSoftware.OLVColumn colmun1;
+        private BrightIdeasSoftware.OLVColumn column2;
     }
 }
 
--- Everywin/Form1.cs
+++ Everywin/Form1.cs
@@ -9,12 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using BrightIdeasSoftware;
+using System.IO;
+using HWND = System.IntPtr;
 
 namespace Everywin
 {
     public partial class Form1 : Form
     {
-        bool setting_show_thumbnails = false;
         Windows windows;
 
         public Form1()
@@ -25,107 +27,9 @@ namespace Everywin
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            windows = new Windows(WindowsListbox, search_bar);
+            windows = new Windows(search_bar, windows_olv);
             windows.Populate();
-        }
 
-        private void search_bar_TextChanged(object sender, EventArgs e)
-        {            
-            windows.Search(((TextBox)sender).Text);
-        }
-
-        private void WindowsListbox_KeyDown(object sender, KeyEventArgs e)
-        {
-            // CTRL + F
-            if (e.KeyCode == Keys.F && e.Control)
-            {
-                // focus on search bar
-                search_bar.Focus();
-
-                // unselect listbox
-                WindowsListbox.ClearSelected();
-                //WindowsListbox.SetSelected(WindowsListbox.SelectedIndex, false);
-            }
-
-            if (e.KeyCode == Keys.Enter)
-            {
-                windows.Enter();
-            }
-        }
-
-        private void WindowsListbox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (!setting_show_thumbnails)
-            {
-                return;
-            }
-
-            IntPtr thumb;
-
-            Windows.WindowEntry selected = (Windows.WindowEntry)WindowsListbox.SelectedItem;
-
-            int i = DwmRegisterThumbnail(WindowsListbox.FindForm().Handle, selected.GetHandle(), out thumb);
-
-            DWM_THUMBNAIL_PROPERTIES props = new DWM_THUMBNAIL_PROPERTIES();
-
-            props.fVisible = true;
-            props.dwFlags = DWM_TNP_VISIBLE | DWM_TNP_RECTDESTINATION | DWM_TNP_OPACITY;
-            props.opacity = 255;
-            //props.rcDestination = new Rect(preview_picbox.Left, preview_picbox.Top, preview_picbox.Right, preview_picbox.Bottom);
-            props.rcDestination = new Rect(0, 0,1000, 1000);
-            DwmUpdateThumbnailProperties(thumb, ref props);
-        }
-
-        [DllImport("dwmapi.dll")]
-        static extern int DwmRegisterThumbnail(IntPtr dest, IntPtr src, out IntPtr thumb);
-
-        [DllImport("dwmapi.dll")]
-        static extern int DwmUpdateThumbnailProperties(IntPtr hThumb, ref DWM_THUMBNAIL_PROPERTIES props);
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct DWM_THUMBNAIL_PROPERTIES
-        {
-            public int dwFlags;
-            public Rect rcDestination;
-            public Rect rcSource;
-            public byte opacity;
-            public bool fVisible;
-            public bool fSourceClientAreaOnly;
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct Rect
-        {
-            internal Rect(int left, int top, int right, int bottom)
-            {
-                Left = left;
-                Top = top;
-                Right = right;
-                Bottom = bottom;
-            }
-
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct PSIZE
-        {
-            public int x;
-            public int y;
-        }
-
-        static readonly int DWM_TNP_VISIBLE = 0x8;
-        static readonly int DWM_TNP_OPACITY = 0x4;
-        static readonly int DWM_TNP_RECTDESTINATION = 0x1;
-
-        private void search_bar_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Down)
-            {
-                WindowsListbox.Focus();
-                WindowsListbox.SelectedIndex = 0;
-            }
         }
 
         protected override bool ProcessDialogKey(Keys keyData)
@@ -137,63 +41,121 @@ namespace Everywin
             }
             return base.ProcessDialogKey(keyData);
         }
+
+        private void windows_olv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void windows_olv_KeyDown(object sender, KeyEventArgs e)
+        {
+            //// CTRL + F
+            if (e.KeyData == Keys.F && e.Control)
+            {
+                // focus on search bar
+                search_bar.Focus();
+
+                // unselect listbox
+                //windows_olv.SelectObject(null);
+                //WindowsListbox.SetSelected(WindowsListbox.SelectedIndex, false);
+            }
+        }
+
+        private void windows_olv_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                windows.Enter();
+            }
+
+            if (e.KeyChar != (char)Keys.Up && e.KeyChar != (char)Keys.Down)
+            {
+                search_bar.Focus();
+
+                // send keys to search_bar.
+                // search bar will bring back focus to us
+                SendKeys.Send(e.KeyChar.ToString());
+
+                //windows_olv.Focus();
+            }
+        }
+
+        private void windows_olv_MouseClick(object sender, MouseEventArgs e)
+        {
+            windows.Enter();
+        }
+
+        private void search_bar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void search_bar_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                windows.Enter();
+            }
+        }
+
+        private void search_bar_TextChanged(object sender, EventArgs e)
+        {
+            windows.Search(((TextBox)sender).Text);
+        }
+
+        private void search_bar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                windows_olv.Focus();
+                windows_olv.SelectedIndex = 0;
+            }
+        }
     }
 
     public class Windows
     {
-        
-
-        private ListBox listbox;
         private TextBox textbox;
         private List<WindowEntry> windows_list;
-        private int text_x = 0;
-        private int text_y = 0;
         private Point text_pos = new Point(0, 0);
         private string current_search_text = "";
-        private bool may_draw_gui = false;
-        private bool should_draw = false;
+        private ObjectListView olv;
 
         public class WindowEntry
         {
             private string title;
             private IntPtr handle;
-            private int matching_title_part_index;
             public bool was_drawn = false;
+            private string image;
+            private string process_path;
 
-            public WindowEntry(string window_title, IntPtr window_handle)
+            public WindowEntry(string window_title, IntPtr window_handle, string process_path)
             {
                 title = window_title;
                 handle = window_handle;
-                matching_title_part_index = -1;
+                this.process_path = process_path;
+                this.image = Path.GetFileName(process_path);
+
             }
 
-            public string GetTitle()
+            public string ProcessPath
             {
-                return title;
+                get { return process_path; }
+            }
+
+            public string Image
+            {
+                get { return image; }
+            }
+
+            public string Title
+            {
+                get { return title; }
             }
 
             public IntPtr GetHandle()
             {
                 return handle;
-            }
-
-            public int match_search(string to_match)
-            {
-                if (to_match == "")
-                {
-                    matching_title_part_index = -1;
-                }
-                else
-                {
-                    matching_title_part_index = title.ToLower().IndexOf(to_match.ToLower());
-                }
-                
-                return matching_title_part_index;
-            }
-
-            public int MatchingPartIndex()
-            {
-                return matching_title_part_index;
             }
 
             public override string ToString()
@@ -202,64 +164,63 @@ namespace Everywin
             }
         }
 
-        public Windows(ListBox window_listbox, TextBox search_textbox)
+        public Windows(TextBox search_textbox, ObjectListView olv)
         {
-            listbox = window_listbox;
-            textbox = search_textbox;
-
-            // set listbox to use our drawing function (for bold text..)
-            //listbox.DrawMode = DrawMode.OwnerDrawFixed;
-            listbox.DrawItem += new DrawItemEventHandler(windows_listbox_drawitem);
+            this.textbox = search_textbox;
+            this.olv = olv;
         }
 
         public void Populate()
         {
             windows_list = new List<WindowEntry>();
+            ImageList icons = new ImageList();
 
-            Process[] processlist = Process.GetProcesses();
+            //Process[] processlist = Process.GetProcesses();
 
-            foreach (Process process in processlist)
+            //foreach (Process process in processlist)
+            //{
+            //    if (!String.IsNullOrEmpty(process.MainWindowTitle))
+            //    {
+            //        windows_list.Add(new WindowEntry(process.MainWindowTitle, process.MainWindowHandle, process.MainModule.FileName));
+            //        Icon icon = Icon.ExtractAssociatedIcon(process.MainModule.FileName);
+            //        icons.Images.Add(process.MainModule.FileName, icon);
+            //        //Console.WriteLine("Process: {0} ID: {1} Window title: {2}", process.ProcessName, process.Id, process.MainWindowTitle);
+            //    }
+            //}
+
+            windows_list = OpenWindowGetter.GetOpenWindows();
+
+            foreach (WindowEntry win in windows_list)
             {
-                if (!String.IsNullOrEmpty(process.MainWindowTitle))
-                {
-                    windows_list.Add(new WindowEntry(process.MainWindowTitle, process.MainWindowHandle));
-
-                    //Console.WriteLine("Process: {0} ID: {1} Window title: {2}", process.ProcessName, process.Id, process.MainWindowTitle);
-                }
+                Icon icon = Icon.ExtractAssociatedIcon(win.ProcessPath);
+                icons.Images.Add(win.ProcessPath, icon);
             }
 
-            UpdateInterface();
+            olv.SmallImageList = icons;
+            olv.SetObjects(windows_list);
         }
 
         public void Search(string text)
         {
-            may_draw_gui = false;
-            foreach (WindowEntry win in windows_list)
-            {
-                win.match_search(text);
-            }
+            //foreach (WindowEntry win in windows_list)
+            //{
+            //    win.match_search(text);
+            //}
 
             current_search_text = text;
-          
-            UpdateInterface();
 
-            may_draw_gui = true;
-        }
+            TextMatchFilter filter = TextMatchFilter.Contains(olv, current_search_text);
 
-        public void UpdateInterface()
-        {
+            olv.ModelFilter = filter;
 
-            listbox.Items.Clear();
-            foreach (WindowEntry win in windows_list)
-            {
-                if (win.MatchingPartIndex() != -1  || current_search_text == "")
-                {
-                    win.was_drawn = false;
-                    listbox.Items.Add(win);
-                }
-            }
+            olv.SelectedIndex = 0;
+            
+            //// Text highlighting requires at least a default renderer
+            //if (olv.DefaultRenderer == null)
+            //    olv.DefaultRenderer = new HighlightTextRenderer(filter);
 
-            //listbox.Update();
+            //olv.SetObjects(windows_list);
+            //UpdateInterface();
 
         }
 
@@ -270,120 +231,88 @@ namespace Everywin
         {
             WindowEntry selected_win;
 
-            selected_win = (WindowEntry)listbox.SelectedItem;
+            selected_win = (WindowEntry) olv.SelectedObject;
 
             SetForegroundWindow(selected_win.GetHandle());
 
-            listbox.FindForm().Close();
+            olv.FindForm().Close();
         }
 
-        private void windows_listbox_drawitem(object sender, DrawItemEventArgs e)
+
+        /// <summary>Contains functionality to get all the open windows.</summary>
+        public static class OpenWindowGetter
         {
-            
-            WindowEntry win_entry = (WindowEntry)((ListBox)(sender)).Items[e.Index];
-
-            //if (((ListBox)(sender)).SelectedIndex != e.Index)
-            //{
-            //    if (win_entry.was_drawn)
-            //    {
-
-            //        e.Graphics.Clear(e.BackColor);
-            //        //e.DrawBackground();
-            //        //e.DrawFocusRectangle();
-            //        #return;
-            //    }
-            //}
-
-            if (win_entry.was_drawn)
+            /// <summary>Returns a dictionary that contains the handle and title of all the open windows.</summary>
+            /// <returns>A dictionary that contains the handle and title of all the open windows.</returns>
+            public static List<WindowEntry> GetOpenWindows()
             {
+                HWND shellWindow = GetShellWindow();
+                List<WindowEntry> open_windows = new List<WindowEntry>();
+    
+            EnumWindows(delegate (HWND hWnd, int lParam)
+            {
+                if (hWnd == shellWindow) return true;
+                if (!IsWindowVisible(hWnd)) return true;
 
-                //e.Graphics.Clear(e.BackColor);
-                //e.DrawBackground();
-                //e.DrawFocusRectangle();
-                //return;
+                int length = GetWindowTextLength(hWnd);
+                if (length == 0) return true;
+
+                StringBuilder builder = new StringBuilder(length);
+                GetWindowText(hWnd, builder, length + 1);
+
+                string title = builder.ToString();
+
+                string path = GetProcessPath(hWnd);
+
+                open_windows.Add(new WindowEntry(title, hWnd, path));
+
+                return true;
+
+            }, 0);
+
+                return open_windows;
             }
 
-            Font matching_font = new Font("Arial", 10, FontStyle.Bold);
-            Font regular_font = new Font("Arial", 10, FontStyle.Regular);
-            
-            string full_text = ((ListBox)(sender)).Items[e.Index].ToString();
-
-            string[] text_parts;
-            Font[] text_fonts;
-
-            int bold_index = win_entry.MatchingPartIndex();
-
-            if (bold_index != -1)
+            //WARN: Only for "Any CPU":
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern int GetWindowThreadProcessId(IntPtr handle, out uint processId);
+            public static string GetProcessPath(IntPtr hwnd)
             {
-                // SPLIT FOR 3 PARTS
-                text_parts = new string[] 
-                {   full_text.Substring(0, bold_index),
-                    full_text.Substring(bold_index, current_search_text.Length),
-                    full_text.Substring(bold_index + current_search_text.Length),
-                };
-
-                text_fonts = new Font[]
+                uint pid = 0;
+                GetWindowThreadProcessId(hwnd, out pid);
+                if (hwnd != IntPtr.Zero)
                 {
-                    regular_font,
-                    matching_font,
-                    regular_font
-                };
-
-            }
-            else
-            {
-                // ONE PART
-                text_parts = new string[]
-                {
-                    full_text
-                };
-
-                text_fonts = new Font[]
-                {
-                    regular_font
-                };
-
-                //TextRenderer.DrawText(e.Graphics, text, font, text_rec, SystemColors.ControlText);
-            }
-
-            /*
-             * 
-             * DRAW TEXT PARTS
-             * 
-            */
-
-            e.DrawBackground();
-
-            Rectangle text_rec = new Rectangle(e.Bounds.Location, new Size(0, 0));
-
-            foreach (int i in Enumerable.Range(0, text_parts.Length))
-             {
-                Font font = text_fonts[i];
-                string part = text_parts[i];
-                Size part_size = TextRenderer.MeasureText(part, font);
-
-                if (part_size.Width >= 5)
-                {
-                    part_size.Width -= 5;
+                    if (pid != 0)
+                    {
+                        var process = Process.GetProcessById((int)pid);
+                        if (process != null)
+                        {
+                            return process.MainModule.FileName.ToString();
+                        }
+                    }
                 }
-                
-                text_rec.Size = part_size;
-
-                TextRenderer.DrawText(e.Graphics,
-                part,
-                font,
-                text_rec,
-                SystemColors.ControlText);
-
-                text_rec.Location += new Size(part_size.Width, 0);
+                return "";
             }
-            
-            e.DrawFocusRectangle();
+            private delegate bool EnumWindowsProc(HWND hWnd, int lParam);
 
-            //e.Graphics.DrawString(WindowsListbox.Items[e.Index].ToString(), new Font("Arial", 10, FontStyle.Bold), Brushes.Black, e.Bounds);
-            win_entry.was_drawn = true;
+            [DllImport("USER32.DLL")]
+            private static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
 
+            [DllImport("USER32.DLL" ,CharSet = CharSet.Unicode)]
+            private static extern int GetWindowText(HWND hWnd, StringBuilder lpString, int nMaxCount);
+
+            [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+            private static extern int GetWindowTextLength(HWND hWnd);
+
+            [DllImport("USER32.DLL")]
+            private static extern bool IsWindowVisible(HWND hWnd);
+
+            [DllImport("USER32.DLL")]
+            private static extern IntPtr GetShellWindow();
         }
     }
+
     
+
 }
+
